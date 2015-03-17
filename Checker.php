2d65688b239c2f530ed2fad655791f8af3f78988
bootjp <oh@bootjp.me>
2015-03-17 15:18:16 +0900

    useragent set

--- Checker.php
+++ Checker.php
@@ -28,6 +28,7 @@ class Checker
         $this->doubleCheck = (bool) $doubleCheck;
         $this->client = new \GuzzleHttp\Client();
         $this->client->setDefaultOption('exceptions', false);
+        $this->client->setDefaultOption('headers', ['User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36']);
     }
     /**
      * Wrapper
